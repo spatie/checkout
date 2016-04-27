4a49349fb4453f62d41cfcbed96133284766d4f7
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-04-27 11:22:30 +0200

    IoC fix

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -8,9 +8,7 @@ class CheckoutServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(Checkout::class, function () {
-            return $this->app->make('Spatie\Checkout\Checkout');
-        });
+        $this->app->singleton(Checkout::class);
 
         $this->app->alias(Checkout::class, 'checkout');
     }
